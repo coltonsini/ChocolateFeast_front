7fcd36b820cc47a3f5f23413f339efd053b89a9d
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2022-06-23 12:44:16 -0500

    actualizar

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const API_URL = 'https://chocolatefeast.herokuapp.com';
 
 
-const HTMLResponse = document.querySelector("#dinero1");
-const ul = document.createDocumentFragment();
+// const HTMLResponse = document.querySelector("#dinero1");
+// const ul = document.createDocumentFragment();
 
-fetch( `${API_URL}/api/casos/`)
-    .then((response) => response.json())
-    .then((casos) => {
-        Array.from(casos).forEach(caso => {
-            let elem = document.createElement('label');
-            elem.appendChild(
-                document.createTextNode(`${caso.n} ${caso.c} ${caso.m}`)
-            );
-            ul.appendChild(elem);
-        })
-        console.log(casos)
+// fetch( `${API_URL}/api/casos/`)
+//     .then((response) => response.json())
+//     .then((casos) => {
+//         Array.from(casos).forEach(caso => {
+//             let elem = document.createElement('label');
+//             elem.appendChild(
+//                 document.createTextNode(`${caso.n} ${caso.c} ${caso.m}`)
+//             );
+//             ul.appendChild(elem);
+//         })
+//         console.log(casos)
        
-        HTMLResponse.appendChild(ul);
-    })
+//         HTMLResponse.appendChild(ul);
+//     })
 
-function asignar(n, c, m){
-    var Dinero = document.getElementById("dinero1")
-    var Chocolate = document.getElementById("chocolate")
-    var Envoltura = document.getElementById("envolturas")
+// function asignar(n, c, m){
+//     var Dinero = document.getElementById("dinero1")
+//     var Chocolate = document.getElementById("chocolate")
+//     var Envoltura = document.getElementById("envolturas")
 
-    Dinero.innerText = `Bobby tiene: ${n} dinero`;
-    Chocolate.innerText = `Bobby tiene: ${c} dinero`;
-    Envoltura.innerText = `Bobby tiene: ${m} dinero`;
-}
-
-
+//     Dinero.innerText = `Bobby tiene: ${n} dinero`;
+//     Chocolate.innerText = `Bobby tiene: ${c} dinero`;
+//     Envoltura.innerText = `Bobby tiene: ${m} dinero`;
+// }
 
 
-function chocolateFeast(n, c, m) {
-    var chocolates = Math.floor(n / c);
-    return chocolates + Math.floor((chocolates - 1) / (m - 1));
-}
+
+
+// function chocolateFeast(n, c, m) {
+//     var chocolates = Math.floor(n / c);
+//     return chocolates + Math.floor((chocolates - 1) / (m - 1));
+// }
